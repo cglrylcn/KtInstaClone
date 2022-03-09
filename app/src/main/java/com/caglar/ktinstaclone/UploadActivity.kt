578619b49088d6a565b485d67c54e9bf29da1e51
Çağlar <caglar.yalcin@isuzu.com.tr>
2022-03-09 15:46:33 +0300

    add firestore upload

--- app/src/main/java/com/caglar/ktinstaclone/UploadActivity.kt
+++ app/src/main/java/com/caglar/ktinstaclone/UploadActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.Toast
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -16,21 +17,74 @@ import androidx.core.content.ContextCompat
 import com.caglar.ktinstaclone.databinding.ActivityMainBinding
 import com.caglar.ktinstaclone.databinding.ActivityUploadBinding
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.Timestamp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+import java.util.*
 
 class UploadActivity : AppCompatActivity() {
     private lateinit var binding: ActivityUploadBinding
     private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
     private lateinit var permissionLauncher: ActivityResultLauncher<String>
     var selectedPicture : Uri?= null
+    private lateinit var auth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+    private lateinit var storage: FirebaseStorage
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUploadBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         registerLauncher()
+        auth = Firebase.auth
+        firestore = Firebase.firestore
+        storage = Firebase.storage
     }
 
     fun upload(view: View) {
+        val reference = storage.reference
+        val uuid = UUID.randomUUID()
+        val imageName = "${uuid}.jpg"
+        val imageReference = reference.child("images/").child(imageName)
+
+        if (selectedPicture != null) {
+            imageReference.putFile(selectedPicture!!)
+                .addOnSuccessListener {
+                    //Save downloadurl -> fireStore
+                    val uploadPictureReference = storage.reference.child("images/").child(imageName)
+                    uploadPictureReference.downloadUrl.addOnSuccessListener {
+                        val downloadUrl = it.toString()
+                        if (auth.currentUser != null) {
+                            val postMap = hashMapOf<String, Any>()
+                            postMap["downloadUrl"] = downloadUrl
+                            postMap["userEmail"] = auth.currentUser!!.email!!
+                            postMap["comment"] = binding.commentText.text.toString()
+                            postMap["date"] = Timestamp.now()
+
+                            firestore.collection("Posts")
+                                .add(postMap)
+                                .addOnSuccessListener {
+                                    finish()
+                                }
+                                .addOnFailureListener {
+                                    Toast.makeText(this,it.localizedMessage,Toast.LENGTH_LONG).show()
+                                }
+                        }
+                    }
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this,it.localizedMessage,Toast.LENGTH_LONG).show()
+                }
+        }
+        else {
+            Toast.makeText(this,"Please select an image",Toast.LENGTH_LONG).show()
+        }
 
     }
 
